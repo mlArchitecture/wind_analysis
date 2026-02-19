f7d895686c56fd75e32d47fe2bddd76b73a0b0e4
rupesh sahoo <sahoosanjita809@gmail.com>
2026-02-19 11:27:19 +0530

    updated

--- Frontend/src/components/montecarlo.jsx
+++ Frontend/src/components/montecarlo.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import styles from "./monteCarlo.module.css";
+import styles from "./montecarlo.module.css";
 
 export default function MonteCarlo() {
   // State management
